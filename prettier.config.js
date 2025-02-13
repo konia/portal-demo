9e775a218c3a22a800b8130cfae6f58441ce3651
konia <huoqihua@cn.ibm.com>
2025-02-14 00:23:14 +0800

    update

--- prettier.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ['@repo/prettier-config/index.js']
-};
--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
 
-import './App.css'
+import './App.css';
 
-import viteLogo from '/vite.svg'
+import viteLogo from '/vite.svg';
+import { Header } from '@repo/ui/header';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const sum = (a: number, b: number): number => {
+    return a + b;
+  };
+
+  sum(1, 2);
 
   return (
     <>
+      <Header title="Hello World"></Header>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        protal
-<a href="https://react.dev" target="_blank">
+        portal
+        <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
